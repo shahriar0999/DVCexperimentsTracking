afb99a4dbc00598f70e0f362e34a7acf52a8018d
shahriar0999 <shahriar0999@proton.me>
2025-05-15 00:12:22 +0600

    modify model_training

--- src/model_training.py
+++ src/model_training.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score, f1_score
 import dvclive
 from dvclive import Live
 
@@ -22,21 +22,26 @@ model.fit(X_train, y_train)
 
 # Make predictions
 y_pred = model.predict(X_test)
+y_pred_proba = model.predict_proba(X_test)[:, 1]
 
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_pred_proba)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+f1 = f1_score(y_test, y_pred, average='weighted')
+
 
 print(f"Accuracy: {accuracy:.4f}")
-print("Classification Report:")
-print(report)
-    
 
 
 with Live(save_dvc_exp=True) as live:
     # Log scalar metrics
     live.log_metric("accuracy", accuracy)
-    live.log_metric("classification_report", report)
+    live.log_metric("roc_auc", roc_auc)
+    live.log_metric("precision", precision)
+    live.log_metric("recall", recall)
+    live.log_metric("f1", f1)
     
     live.log_param("n_estimators", n_estimators)
     live.log_param("max_depth", max_depth)

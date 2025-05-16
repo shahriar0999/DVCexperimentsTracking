e5119233fd4a0f1500eebd97af6b1ef9319d7920
shahriar0999 <shahriar0999@proton.me>
2025-05-16 11:22:54 +0600

    best experiment:

--- data/data.py
+++ data/data.py
@@ -2,6 +2,8 @@ import seaborn as sns
 import pandas as pd
 
 iris = sns.load_dataset('iris')
-iris["species"] = iris["species"].map({"setosa":1, "versicolor":2, "virginica":3})
+iris = iris[iris['species'].isin(['setosa','virginica'])]
+iris["species"] = iris["species"].map({"setosa":0, "virginica":1})
+print(iris.head())
 iris.to_csv('iris.csv', index=False)
 print("Iris Data saved")
--- src/model_training.py
+++ src/model_training.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score, f1_score
 import dvclive
 from dvclive import Live
+from sklearn.neighbors import KNeighborsClassifier
 
 df = pd.read_csv("./data/iris.csv")
 
@@ -13,16 +14,19 @@ y = df.iloc[:, -1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=2, stratify=y)
 
-n_estimators=2
-max_depth=2
+# Hyperparameters
+n_neighbors = 3
+weights = 'distance'
+algorithm = 'kd_tree'
 
-# Initialize and train the Gradient Boosting model
-model = GradientBoostingClassifier(n_estimators=n_estimators, max_depth=max_depth)
-model.fit(X_train, y_train)
+#apply KNN
+knn = KNeighborsClassifier(n_neighbors = n_neighbors, weights= weights ,algorithm=algorithm)
+
 
 # Make predictions
-y_pred = model.predict(X_test)
-y_pred_proba = model.predict_proba(X_test)[:, 1]
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+y_pred_proba = knn.predict_proba(X_test)[:, 1]
 
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
